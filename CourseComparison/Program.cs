13ae027b74eb906eece1f3a4f61c4671a07e153e
unknown <alesfluiz@gmail.com>
2023-05-21 14:20:51 -0300

    Update Lambda

--- CourseComparison/Program.cs
+++ CourseComparison/Program.cs
@@ -12,10 +12,8 @@ namespace CourseComparison
             list.Add(new Product("TV", 900.00));
             list.Add(new Product("Natebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
-
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-            list.Sort(comp);
+                       
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach(Product p in list)
             {
