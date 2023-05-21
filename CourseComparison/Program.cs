9d981c2ae853bfe628813ee4f97be567bfac8029
unknown <alesfluiz@gmail.com>
2023-05-21 14:15:35 -0300

    first commit

---
+++ CourseComparison/Program.cs
@@ -0,0 +1,30 @@
+﻿using CourseComparison.Entities;
+using System;
+
+namespace CourseComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Natebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort(CompareProduct);
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+                        
+        }
+
+        static int CompareProduct(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+    }
+}
